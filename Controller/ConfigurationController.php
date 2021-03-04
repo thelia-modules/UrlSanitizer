c756d6815c43051db7e1bf885b45626fc030bc2e
Vincent Lopes-Vicente <vlopes@openstudio.fr>
2021-03-04 10:57:21 +0100

    New version for 2.5

--- Controller/ConfigurationController.php
+++ Controller/ConfigurationController.php
@@ -7,18 +7,15 @@ use Thelia\Core\Security\AccessManager;
 use Thelia\Core\Security\Resource\AdminResources;
 use Thelia\Tools\URL;
 use UrlSanitizer\Service\UrlSanitizerService;
-use UrlSanitizer\UrlSanitizer;
 
 class ConfigurationController extends BaseAdminController
 {
-    public function sanitizeAllAction()
+    public function sanitizeAllAction(UrlSanitizerService $urlSanitizerService)
     {
         if (null !== $response = $this->checkAuth([AdminResources::MODULE], ["UrlSanitizer"], AccessManager::UPDATE)) {
             return $response;
         }
 
-        /** @var UrlSanitizerService $urlSanitizerService */
-        $urlSanitizerService = $this->getContainer()->get('urlsanitizer.service');
         $urlSanitizerService->sanitizeAllExistingUrls();
 
         return $this->generateRedirect(URL::getInstance()->absoluteUrl('/admin/module/UrlSanitizer', []));
--- UrlSanitizer.php
+++ UrlSanitizer.php
@@ -2,13 +2,22 @@
 
 namespace UrlSanitizer;
 
-use Propel\Runtime\ActiveQuery\Criteria;
-use Thelia\Model\RewritingUrl;
-use Thelia\Model\RewritingUrlQuery;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Thelia\Module\BaseModule;
 
 class UrlSanitizer extends BaseModule
 {
     /** @var string */
     const DOMAIN_NAME = 'urlsanitizer';
+
+    /**
+     * Defines how services are loaded in your modules.
+     */
+    public static function configureServices(ServicesConfigurator $servicesConfigurator): void
+    {
+        $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
+            ->exclude([THELIA_MODULE_DIR.ucfirst(self::getModuleCode()).'/I18n/*'])
+            ->autowire(true)
+            ->autoconfigure(true);
+    }
 }
